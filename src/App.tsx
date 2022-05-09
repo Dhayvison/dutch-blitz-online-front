0364a15b73ffd15016ca1bb729e74b5d3a46ab2a
Dhayvison <dhayvisonbraga@hotmail.com>
2022-05-09 20:57:17 -0300

    feat: add chakra ui to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-import socketIOClient from "socket.io-client";
-import './App.css';
+import { Button, Center, Text, useColorMode } from '@chakra-ui/react';
+import socketIOClient from 'socket.io-client';
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   React.useEffect(() => {
     const socket = socketIOClient('http://localhost:3333/');
   }, []);
 
   return (
-    <h1>Hola</h1>
+    <Center h='100vh' color='white'>
+      <Button onClick={toggleColorMode}>{colorMode === 'light' ? '🌙' : '☀'}</Button>
+    </Center>
   );
 }
 
