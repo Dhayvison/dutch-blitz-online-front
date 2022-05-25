5ff816067fc70f8f7247f8b76eab484b2dd802a0
Dhayvison <dhayvisonbraga@hotmail.com>
2022-05-24 21:27:11 -0300

    feat: add chat drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,94 @@
 import * as React from 'react';
-import { Button, Center, Text, useColorMode } from '@chakra-ui/react';
+import {
+  Alert,
+  Badge,
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  InputRightElement,
+  Stack,
+  Text,
+  useDisclosure,
+  Wrap,
+} from '@chakra-ui/react';
 import socketIOClient from 'socket.io-client';
+import { ChatIcon } from '@chakra-ui/icons';
 
 function App() {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  function onSubmitMessage(event: any) {
+    event.preventDefault();
+  }
 
   React.useEffect(() => {
-    const socket = socketIOClient('http://localhost:3333/');
+    const socket = socketIOClient(process.env.REACT_APP_API_URL as string);
   }, []);
 
   return (
-    <Center h='100vh' color='white'>
-      <Button onClick={toggleColorMode}>{colorMode === 'light' ? '🌙' : '☀'}</Button>
-    </Center>
+    <>
+      <IconButton
+        position='fixed'
+        bottom={30}
+        right={30}
+        boxShadow='md'
+        colorScheme='blue'
+        size='lg'
+        aria-label='Search database'
+        icon={<ChatIcon />}
+        onClick={onOpen}
+      />
+      <Drawer isOpen={isOpen} placement='right' onClose={onClose} size='md'>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Chat</DrawerHeader>
+
+          <DrawerBody>
+            <Stack spacing={4}>
+              <Alert
+                status='info'
+                variant='left-accent'
+                borderRadius='base'
+                boxShadow='base'
+                bg='transparent'
+              >
+                <Wrap>
+                  <Badge>John Doe</Badge>
+                  <Text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus illo perspiciatis
+                    voluptatibus iure fugiat debitis praesentium fuga eligendi, et dignissimos id
+                    error iusto, explicabo sequi eaque earum qui assumenda doloremque?
+                  </Text>
+                </Wrap>
+              </Alert>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Box as='form' w='100%' onSubmit={onSubmitMessage}>
+              <FormControl variant='floating' id='message'>
+                <Input placeholder=' ' isRequired autoComplete='off' />
+                <FormLabel>Escreva sua mensagem</FormLabel>
+                <InputRightElement width='4.5rem'>
+                  <Button type='submit'>Enviar</Button>
+                </InputRightElement>
+              </FormControl>
+            </Box>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
+const activeLabelStyles = {
+  transform: 'scale(0.85) translateY(-24px)',
+};
+
+const theme = extendTheme({
+  components: {
+    Form: {
+      variants: {
+        floating: {
+          container: {
+            _focusWithin: {
+              label: {
+                ...activeLabelStyles,
+              },
+            },
+            'input:not(:placeholder-shown) + label, .chakra-select__wrapper + label': {
+              ...activeLabelStyles,
+            },
+            label: {
+              top: 0,
+              left: 0,
+              zIndex: 2,
+              position: 'absolute',
+              backgroundColor: 'white',
+              pointerEvents: 'none',
+              mx: 3,
+              px: 2,
+              my: 2,
+              transformOrigin: 'left top',
+              borderRadius: '50%',
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
