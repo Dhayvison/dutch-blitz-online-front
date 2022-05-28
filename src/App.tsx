aabdd372875d2b5612e4644ce8cf309c8969629e
Dhayvison <dhayvisonbraga@hotmail.com>
2022-05-28 17:51:09 -0300

    feat: implements connect to socket chat

--- src/App.tsx
+++ src/App.tsx
@@ -21,18 +21,72 @@ import {
   useDisclosure,
   Wrap,
 } from '@chakra-ui/react';
-import socketIOClient from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 import { ChatIcon } from '@chakra-ui/icons';
+
+type ChatMessageProps = {
+  userName: string;
+  message: string;
+};
+
+type ChatMessage = {
+  id: string;
+  user: ChatUser;
+  text: string;
+  time: number;
+};
+
+type ChatUser = {
+  id: string;
+  name: string;
+};
+
+function ChatMessage({ userName, message }: ChatMessageProps) {
+  return (
+    <Alert
+      status='info'
+      variant='left-accent'
+      borderRadius='base'
+      boxShadow='base'
+      bg='transparent'
+    >
+      <Wrap direction='column'>
+        <Badge w='min-content'>{userName}</Badge>
+        <Text>{message}</Text>
+      </Wrap>
+    </Alert>
+  );
+}
 
 function App() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  const [message, setMessage] = React.useState('');
+  const [socket, setSocket] = React.useState<Socket>();
+  const [messages, setMessages] = React.useState<Array<ChatMessageProps>>([]);
+
   function onSubmitMessage(event: any) {
     event.preventDefault();
+    socket?.emit('chat message', { message });
+  }
+
+  function handleChangeMessage(event: any) {
+    setMessage(event.target.value);
+  }
+
+  function handleReceptMessage(message: ChatMessageProps) {
+    setMessages((currentMessages) => {
+      console.log([...currentMessages, message]);
+      return [...currentMessages, message];
+    });
   }
 
   React.useEffect(() => {
-    const socket = socketIOClient(process.env.REACT_APP_API_URL as string);
+    const newSocket = io(process.env.REACT_APP_API_URL as string);
+    setSocket(newSocket);
+    return () => {
+      newSocket.close();
+    };
   }, []);
 
   return (
@@ -56,32 +110,34 @@ function App() {
 
           <DrawerBody>
             <Stack spacing={4}>
-              <Alert
-                status='info'
-                variant='left-accent'
-                borderRadius='base'
-                boxShadow='base'
-                bg='transparent'
-              >
-                <Wrap>
-                  <Badge>John Doe</Badge>
-                  <Text>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus illo perspiciatis
-                    voluptatibus iure fugiat debitis praesentium fuga eligendi, et dignissimos id
-                    error iusto, explicabo sequi eaque earum qui assumenda doloremque?
-                  </Text>
-                </Wrap>
-              </Alert>
+              {messages.map((itemMessage) => {
+                return (
+                  <ChatMessage
+                    key={itemMessage.message}
+                    userName={itemMessage.userName}
+                    message={itemMessage.message}
+                  ></ChatMessage>
+                );
+              })}
             </Stack>
           </DrawerBody>
 
           <DrawerFooter>
             <Box as='form' w='100%' onSubmit={onSubmitMessage}>
               <FormControl variant='floating' id='message'>
-                <Input placeholder=' ' isRequired autoComplete='off' />
+                <Input
+                  placeholder=' '
+                  isRequired
+                  autoComplete='off'
+                  colorScheme='blue'
+                  value={message}
+                  onChange={(event) => handleChangeMessage(event)}
+                />
                 <FormLabel>Escreva sua mensagem</FormLabel>
-                <InputRightElement width='4.5rem'>
-                  <Button type='submit'>Enviar</Button>
+                <InputRightElement>
+                  <Button type='submit' colorScheme='blue'>
+                    Enviar
+                  </Button>
                 </InputRightElement>
               </FormControl>
             </Box>
