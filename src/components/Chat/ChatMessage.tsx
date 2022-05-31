657e7af838673f1dde9bcdf54bef2ac5c24f2875
Dhayvison <dhayvisonbraga@hotmail.com>
2022-05-30 21:43:56 -0300

    feat: add behavior scroll to message bottom

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -3,8 +3,18 @@ import { Avatar, Box, Tag, TagLabel, Text } from '@chakra-ui/react';
 import { ChatMessage as ChatMessageType } from '../../types/Chat';
 
 export function ChatMessage({ message }: { message: ChatMessageType }) {
+  const messagesEndRef = React.useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  React.useEffect(() => {
+    scrollToBottom();
+  }, []);
+
   return (
-    <Box borderRadius='base' boxShadow='base'>
+    <Box borderRadius='base' boxShadow='base' ref={messagesEndRef}>
       <Tag>
         <Avatar size='xs' name={message.user.name} mr={2} />
         <TagLabel>{message.user.name}</TagLabel>
