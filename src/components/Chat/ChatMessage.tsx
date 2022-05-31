1d1d4b4fd1f24055daaff12168373ebfc3813004
Dhayvison <dhayvisonbraga@hotmail.com>
2022-05-30 22:26:27 -0300

    chore: add message caret

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -1,6 +1,30 @@
 import * as React from 'react';
-import { Avatar, Box, Tag, TagLabel, Text } from '@chakra-ui/react';
+import { Avatar, Box, Tag, TagLabel, Text, useColorMode, useTheme } from '@chakra-ui/react';
 import { ChatMessage as ChatMessageType } from '../../types/Chat';
+
+function MessageCaret() {
+  const { colorMode } = useColorMode();
+  const theme = useTheme();
+  console.log();
+
+  return (
+    <Box
+      style={{
+        position: 'absolute',
+        top: '4px',
+        right: '100%',
+        left: '-8px',
+        display: 'block',
+        width: '8px',
+        height: '16px',
+        pointerEvents: 'none',
+        content: ' ',
+        clipPath: 'polygon(0 50%, 100% 0, 100% 100%)',
+        background: colorMode === 'light' ? theme.colors.gray['100'] : theme.colors.gray['600'],
+      }}
+    ></Box>
+  );
+}
 
 export function ChatMessage({ message }: { message: ChatMessageType }) {
   const messagesEndRef = React.useRef<HTMLDivElement>(null);
@@ -14,7 +38,8 @@ export function ChatMessage({ message }: { message: ChatMessageType }) {
   }, []);
 
   return (
-    <Box borderRadius='base' boxShadow='base' ref={messagesEndRef}>
+    <Box position='relative' borderRadius='base' boxShadow='base' ref={messagesEndRef}>
+      <MessageCaret />
       <Tag>
         <Avatar size='xs' name={message.user.name} mr={2} />
         <TagLabel>{message.user.name}</TagLabel>
