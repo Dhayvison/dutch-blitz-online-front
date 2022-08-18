db1a46050fc7d54b0235b0771c475558fa0c02a8
Dhayvison <dhayvisonbraga@hotmail.com>
2022-08-18 14:20:51 -0300

    feat: add lib react-error-boundary

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,37 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import themeConfig from './theme-config';
 import App from './App';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 const theme = extendTheme(themeConfig);
 
+type ErrorFDP = {
+  message: string
+}
+
+function ErrorFallback({error, resetErrorBoundary}: {error: ErrorFDP, resetErrorBoundary: any}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        // reset the state of your app so the error doesn't happen again
+      }}
+    >
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
