c49c0a8b95b21c46ecb7c76dcad9cbbc32fec4d5
Dhayvison <dhayvisonbraga@hotmail.com>
2022-08-18 22:16:48 -0300

    feat: define layout of error boundary and actions

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import {
+  Button,
+  Center,
+  ChakraProvider,
+  Code,
+  extendTheme,
+  Heading,
+  Link,
+  Stack,
+  Text,
+  useToast,
+} from '@chakra-ui/react';
 import themeConfig from './theme-config';
 import App from './App';
 import { ErrorBoundary } from 'react-error-boundary';
@@ -9,31 +20,62 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 const theme = extendTheme(themeConfig);
 
-type ErrorFDP = {
-  message: string
-}
+type ErrorF = {
+  message: string;
+};
 
-function ErrorFallback({error, resetErrorBoundary}: {error: ErrorFDP, resetErrorBoundary: any}) {
+function ErrorFallback({
+  error,
+  resetErrorBoundary,
+}: {
+  error: ErrorF;
+  resetErrorBoundary: () => void;
+}) {
+  const toast = useToast({ position: 'bottom-left', isClosable: true });
+  const handleClearPageData = () => {
+    localStorage.clear();
+    toast({
+      title: 'Feito',
+      description: 'Os dados da página foram redefinidos.',
+      status: 'success',
+    });
+    resetErrorBoundary();
+  };
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  )
+    <Center bg='black' h='100vh'>
+      <Stack spacing={6}>
+        <Heading className='glitch' as='h1' size='4xl' noOfLines={1}>
+          <span aria-hidden='true'>500</span>
+          500
+          <span aria-hidden='true'>500</span>
+        </Heading>
+        <Text fontSize='6xl' noOfLines={1}>
+          Oh não! Tivemos um erro inesperado
+        </Text>
+        <Code colorScheme='red'>{error.message}</Code>
+        <Text> Aqui estão algumas ações que podem ajudá-lo:</Text>
+        <Stack direction='row' spacing={4} align='center'>
+          <Button onClick={resetErrorBoundary} variant='solid'>
+            Tentar novamente
+          </Button>
+          <Button onClick={handleClearPageData} variant='ghost'>
+            Limpar dados de navegação
+          </Button>
+          <Link href='/' variant='outline'>
+            Ir para o início
+          </Link>
+        </Stack>
+      </Stack>
+    </Center>
+  );
 }
 
 root.render(
   <React.StrictMode>
-    <ErrorBoundary
-      FallbackComponent={ErrorFallback}
-      onReset={() => {
-        // reset the state of your app so the error doesn't happen again
-      }}
-    >
-      <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme}>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
         <App />
-      </ChakraProvider>
-    </ErrorBoundary>
+      </ErrorBoundary>
+    </ChakraProvider>
   </React.StrictMode>
 );
