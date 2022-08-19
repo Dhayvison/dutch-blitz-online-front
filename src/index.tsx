0a7bffc2d0fb6170e7d39855df0bcf1bc4ed7111
Dhayvison <dhayvisonbraga@hotmail.com>
2022-08-19 13:40:04 -0300

    feat: add global ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -11,26 +11,17 @@ import {
   Stack,
   Text,
   useToast,
+  Wrap,
 } from '@chakra-ui/react';
 import themeConfig from './theme-config';
 import App from './App';
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 const theme = extendTheme(themeConfig);
 
-type ErrorF = {
-  message: string;
-};
-
-function ErrorFallback({
-  error,
-  resetErrorBoundary,
-}: {
-  error: ErrorF;
-  resetErrorBoundary: () => void;
-}) {
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
   const toast = useToast({ position: 'bottom-left', isClosable: true });
   const handleClearPageData = () => {
     localStorage.clear();
@@ -43,29 +34,32 @@ function ErrorFallback({
   };
   return (
     <Center bg='black' h='100vh'>
-      <Stack spacing={6}>
-        <Heading className='glitch' as='h1' size='4xl' noOfLines={1}>
-          <span aria-hidden='true'>500</span>
-          500
-          <span aria-hidden='true'>500</span>
-        </Heading>
-        <Text fontSize='6xl' noOfLines={1}>
-          Oh não! Tivemos um erro inesperado
-        </Text>
-        <Code colorScheme='red'>{error.message}</Code>
-        <Text> Aqui estão algumas ações que podem ajudá-lo:</Text>
-        <Stack direction='row' spacing={4} align='center'>
-          <Button onClick={resetErrorBoundary} variant='solid'>
-            Tentar novamente
-          </Button>
-          <Button onClick={handleClearPageData} variant='ghost'>
-            Limpar dados de navegação
-          </Button>
-          <Link href='/' variant='outline'>
-            Ir para o início
-          </Link>
+      <Wrap padding='0 200px 200px 0' bg='url(/assets/images/logo_db.png)' >
+        <Stack spacing={6} p={6} borderWidth='1px' bg='blackAlpha.800' minW='50vw'>
+          <Heading className='glitch' as='h1' size='4xl' noOfLines={1}>
+            <span aria-hidden='true'>500</span>
+            500
+            <span aria-hidden='true'>500</span>
+          </Heading>
+          <Text fontSize='4xl' noOfLines={1} className='shake shake-hard'>
+            Oh não! Tivemos um erro inesperado
+          </Text>
+          
+          <Code colorScheme='red'>{error.message}</Code>
+          <Text> Aqui estão algumas ações que podem ajudá-lo:</Text>
+          <Stack direction='row' spacing={4} align='center'>
+            <Button onClick={resetErrorBoundary} variant='solid'>
+              Tentar novamente
+            </Button>
+            <Button onClick={handleClearPageData} variant='ghost'>
+              Limpar dados de navegação
+            </Button>
+            <Link href='/' variant='outline'>
+              Ir para o início
+            </Link>
+          </Stack>
         </Stack>
-      </Stack>
+      </Wrap>
     </Center>
   );
 }
